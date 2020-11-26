81aee69f27d3176d2dc79225852aeaf2ad8b7f45
GiaKhai <khaigia11111@gmail.com>
2020-11-26 18:15:54 +0700

    second commit

---
+++ src/HeaderContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./headerContent.css"
+
+class HeaderContent extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default HeaderContent;
